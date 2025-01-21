ce1ff567b1eea783e0a43f7a2fcd2590986b36da
rlaals0203 <scientistmin@gmail.com>
2025-01-21 20:40:04 +0900

    feat : custom seat generator window

--- Assets/Script/Editor/SeatGeneratorEditor.cs
+++ Assets/Script/Editor/SeatGeneratorEditor.cs
@@ -1,22 +1,61 @@
+using System;
 using UnityEngine;
 using UnityEditor;
 using UnityEngine.UIElements;
+using System.Collections.Generic;
 
-[CustomEditor(typeof(SeatGenerator))]
-public class SeatGeneratorEditor : Editor
+public class SeatGeneratorEditor : EditorWindow
 {
-    public VisualTreeAsset VisualTree;
+    private const int Vert = 40;
+    private const int Horizon = 50;
+    private bool[,] selectedCells = new bool[Vert, Horizon];
 
-    private Button _seatBtn;
-
-    public override VisualElement CreateInspectorGUI()
+    private Button seat;
+    private Button generateSeat;
+    private VisualElement background;
+    private TextField prefabTextField;
+    
+    [MenuItem("Tools/SeatGenerator")]
+    public static void ShowWindow()
     {
-        VisualElement root = new VisualElement();
-        VisualTree.CloneTree(root);
+        SeatGeneratorEditor window = GetWindow<SeatGeneratorEditor>();
+        window.titleContent = new GUIContent("SeatGeneratorEditor");
+    }
 
-        _seatBtn = root.Q<Button>("Seat");
+    public void OnGUI()
+    {
+        
+        
+    }
 
-        return base.CreateInspectorGUI();
+    public void CreateGUI()
+    {
+        VisualElement root = rootVisualElement;
+        VisualTreeAsset asset = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Editor/CustomSeatGenerator.uxml");
+        VisualElement tree = asset.Instantiate();
+        root.Add(tree);
 
+        #region AssignElements
+        seat = tree.Q<Button>("seat");
+        generateSeat = tree.Q<Button>("generate-btn");
+        background = tree.Q<VisualElement>("seat-background");
+        prefabTextField = tree.Q<TextField>("seat-name");
+        #endregion
+        
+        for (int y = 0; y < Vert; y++)
+        {
+            for (int x = 0; x < Horizon; x++)
+            {
+                Button button = new Button();
+                button.name = "seat";
+                button.clicked += OnSeatClicked;
+                background.Add(button);
+            }
+        }
+    }
+
+    private void OnSeatClicked()
+    {
+        
     }
 }
